a273e4dae87718626ce90bbd1c430ea2aa724c05
Naomi7776x <naomi.mello@escola.pr.gov.br>
2025-05-25 16:52:55 -0300

    Create perguntas.js
    Aula 6,7,8,9 e 10

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    '10 perguntas inúteis',
+    '1. Quantos corações polvos tem?',
+    'Os polvos têm três corações.'
+)
+
+criaCartao(
+    '10 perguntas inúteis',
+    '2. As formigas dormem?',
+    'Sim, as formigas dormem, embora de forma diferente dos humanos.'
+)
+
+criaCartao(
+    '10 perguntas inúteis',
+    '3. Como foi feito o som do codzilla?',
+    'foi feito esfregando as cordas de um contrabaixo com uma luva revestida de resina'
+)
